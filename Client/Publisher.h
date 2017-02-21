7665073b347a6015df5b8ebe932f1c273bb26694
zeinabzali <zeinabzali@gmail.com>
2017-02-21 13:12:59 +0330

    Add files via upload

--- Client/Publisher.h
+++ Client/Publisher.h
@@ -21,7 +21,7 @@ public:
     QTcpSocket *peerSocket;
     QFile *headerFile;
     QFile *contentFile;
-    static const int MAX_READ_SIZE = 2000000;//2MB
+    static const int MAX_READ_SIZE = 100000000;//100MB
 signals:
     
 public slots:
--- Client/sICNProvider.h
+++ Client/sICNProvider.h
@@ -29,7 +29,7 @@ public:
     Provider *provider;
     Request request;
     bool success;
-    static const int MAX_READ_SIZE = 2000000;//2MB
+    static const int MAX_READ_SIZE = 100000000;//100MB
 
 signals:
 
